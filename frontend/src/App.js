95ff028e5ef325989dd40a9ca53d5a5db41f26fd
Francisco Cajlon Jhonathan Moura Batista <cajlonbatista@outlook.com.br>
2020-12-03 18:53:30 -0300

    Configure route main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+import Routes from "./routes";
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes/>
+      </BrowserRouter>
     </div>
   );
 }
